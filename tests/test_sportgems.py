72e857a0c465c31196572913170b1c1a0dd359ac
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-12-17 17:45:13 +0000

    improve on python test

--- tests/test_sportgems.py
+++ tests/test_sportgems.py
@@ -16,24 +16,32 @@ def track():
     start_coordinate = (48.0, 8.0)
     # insert a section with 0.001 degree per second
     for i in range(100):
-        next_coordinate = (start_coordinate[0], start_coordinate[1] + i * 0.0001)
+        next_coordinate = (start_coordinate[0], start_coordinate[1] + i * 0.0002)
         track.coordinates.append(next_coordinate)
     # insert a section with 0.003 degree per second which is slightly faster
+    last_coordinate = track.coordinates[-1]
     for i in range(100):
-        next_coordinate = (start_coordinate[0], start_coordinate[1] + i * 0.0003)
+        next_coordinate = (last_coordinate[0], last_coordinate[1] + i * 0.00025)
         track.coordinates.append(next_coordinate)
+    last_coordinate = track.coordinates[-1]
     # insert a section with again 0.001 degree per second
     for i in range(100):
-        next_coordinate = (start_coordinate[0], start_coordinate[1] + i * 0.0001)
+        next_coordinate = (last_coordinate[0], last_coordinate[1] + i * 0.0002)
         track.coordinates.append(next_coordinate)
     for i in range(len(track.coordinates)):
         track.times.append(i)
+    print(f"times: {track.times}")
+    print(f"coordinates: {track.coordinates}")
     assert len(track.times) == len(track.coordinates)
     return track
 
 
 def test_find_gems(track):
-    # print(f"times: {track.times}")
-    # print(f"coordinates: {track.coordinates}")
+    # search for the fastest 1km (=1000m) with the above created track
     fastest_1km = find_gems(1000, track.times, track.coordinates)
-    assert fastest_1km == (99, 112, 77.35165539317262)
+    start_index = fastest_1km[0]
+    end_index = fastest_1km[1]
+    velocity_found = fastest_1km[2]
+    assert start_index == 131
+    assert end_index == 184
+    assert int(velocity_found) == 18
