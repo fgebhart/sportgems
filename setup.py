b574048e8b8318b22442903bf3db8a9f36284132
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-12-18 05:57:51 +0000

    add license and update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from setuptools import setup
@@ -14,14 +15,28 @@ except ImportError:
     else:
         from setuptools_rust import RustExtension
 
+
+def requirements_from_txt(path_to_txt):
+    with open(path_to_txt, "r") as f:
+        reqs = f.readlines()
+    return [req for req in reqs if not req.startswith("#")]
+
+
+with open("Readme.md", "r") as fh:
+    long_description = fh.read()
+
 setup_requires = ['setuptools-rust>=0.6.0']
 install_requires = []
-tests_require = install_requires + ['pytest', 'pytest-benchmark']
+tests_require = install_requires + requirements_from_txt("requirements.txt")
 
 setup(
     name='sportgems',
     author="Fabian Gebhart",
     version=__version__,
+    description="Find valuable gems 💎 in your tracked sport 🚴 activity!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fgebhart/sportgems",
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
@@ -30,14 +45,13 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: MacOS :: MacOS X',
     ],
+    python_requires=">=3.8",
     packages=['sportgems'],
-    url="https://github.com/fgebhart/sportgems",
     rust_extensions=[RustExtension('sportgems._sportgems', 'Cargo.toml')],
     install_requires=install_requires,
-    python_requires=">=3.8",
     tests_require=tests_require,
     setup_requires=setup_requires,
     include_package_data=True,
     zip_safe=False,
-    cmdclass=dict()
+    cmdclass=dict(),
 )
