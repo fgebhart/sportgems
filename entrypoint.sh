93906864bbf74dea6f114576fe0e3fbd3fb483c2
Fabian Gebhart <fabian.gebhart@gmail.com>
2021-02-22 19:46:12 +0100

    fix linker error with workaround

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 source $VIRTUAL_ENV_PATH/bin/activate
+# workaround for linker error when testing, see https://pyo3.rs/v0.13.2/faq.html#i-cant-run-cargo-test-im-having-linker-issues-like-symbol-not-found-or-undefined-reference-to-_pyexc_systemerror
+alias "cargotest"="cargo test --no-default-features"
 zsh
